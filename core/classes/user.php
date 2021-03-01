ddc2f964d5927ee5324681fdfb9bf40eb7e5d512
Ivan Yosifov <ivan_yosifov@yahoo.com>
2021-03-01 13:25:25 +0200

    Login functionality

--- core/classes/user.php
+++ core/classes/user.php
@@ -1,8 +1,54 @@
 <?php
+error_reporting( E_ALL );
 class User {
 	protected $pdo;
 
 	function __construct($pdo){
 		$this->pdo = $pdo;
+	}
+
+	public function checkInput($input){
+		$input = htmlspecialchars($input);
+		$input = trim($input);
+		$input = stripslashes($input);
+
+		return $input;
+	}
+
+	public function login($email, $password){
+		$stmt = $this->pdo->prepare("SELECT `user_id`, `password` FROM users WHERE `email` = :email");
+		$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+		$stmt->execute();
+
+		$user = $stmt->fetch(PDO::FETCH_OBJ);
+		$count = $stmt->rowCount();
+
+		if($count > 0){
+			$hash_password = $user->password;	
+			if(!password_verify($password, $user->password)){
+				return false;
+			}else{
+				$_SESSION['user_id'] = $user->user_id;
+				header('Location: home.php');
+				exit();
+			}
+		}else{
+			return false;
+		}
+		
+	}
+
+	public function userData($user_id){
+		$stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` = :user_id");
+		$stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+		$stmt->execute();
+		return $stmt->fetch(PDO::FETCH_OBJ);
+	}
+
+	public function logout(){
+		$_SESSION = array();
+		session_destroy();
+		header('Location: ../index.php');
+		exit();
 	}
 }
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+$hash_password = password_hash('admin', PASSWORD_DEFAULT);
+
+echo $hash_password;
